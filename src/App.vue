539f6a13aef3215fa389a92d828f3f63667ee564
Tony <geisera@gmail.com>
2022-12-30 08:44:10 -0600

    add banner css to App vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@
 </template>
 
 <style scoped>
+.banner {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
